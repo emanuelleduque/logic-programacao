18023d7a8719a50790e665d2bce9a1f9acd642d7
emanuelleduque <emanuelleduquesantana@gmail.com>
2026-04-14 21:08:13 -0300

    laços de repetição

--- loop/while-04.js
+++ loop/while-04.js
@@ -1,11 +1,10 @@
 const prompt = require('prompt-sync')();
-const diasPrioridade = [];
 const diasPrazo = [];
 const quantidade = [];
 const produto = [];
 const setor = [];
 let opcao = ' ';
-let zonaVermelha = 0; zonaLaranja = 0; zonaAmarela = 0; zonaVerde = 0;
+let zonaVermelha = "Urgente!", zonaLaranja = "Alta", zonaAmarela = "Média", zonaVerde = "Baixa"
 
 
 while(opcao !=="5"){
@@ -18,16 +17,44 @@ while(opcao !=="5"){
 
     opcao = prompt("Escolha uma opção: ");
 
+// CADASTRAR PEDIDOS
  if (opcao === "1"){
     let pedido = prompt("Nome do Produto: ");
     console.log("Produto Cadastrado!")
-    console.log("PED"+produto.length);
+    console.log(`PED${produto.length}`);
 
-    produto.push(produto);
+    produto.push(pedido);
 
-
+// LISTAR PEDIDOS
 } if (opcao === "2"){
     console.log("\n--Lista de Pedidos--");
+
+if (produto.length === 0) {
+        console.log("Nenhum pedido cadastrado no momento.");
+} else {
+    for (let i = 0; i < produto.length; i++) {
+    console.log(`PED${i} | Produto: ${produto[i]}`);}
+} 
+} if (opcao === "3"){
+    console.log("\n--Resumo Gerencial de Pedidos--");
+
+    while(true){
+    let diasPrioridade = parseInt(prompt("Prazo(Dias): "));
+    if(diasPrioridade<0){
+        break;
+    }
+    if (diasPrioridade <= 2){
+        console.log("Prioridade ", zonaVermelha); 
+    } else if (diasPrioridade <= 5){
+        console.log("Prioridade: ", zonaLaranja);
+    } else if (diasPrioridade <= 10){
+      console.log("Prioridade: ", zonaAmarela);
+    } else {
+        console.log("Prioridade: ", zonaVerde);
+    } break;
+}
+
+
 
 
 
@@ -64,4 +91,6 @@ while(opcao !=="5"){
 
 
 }
+
 }
+
