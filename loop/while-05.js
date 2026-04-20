b185d4f78d089f1e16466295a52e610caa3d2d36
emanuelleduque <emanuelleduquesantana@gmail.com>
2026-04-20 15:51:44 -0300

    node alterações e novo menu

---
+++ loop/while-05.js
@@ -0,0 +1,43 @@
+let saldoInicial = 1000.00;
+let extrato = [];
+
+while(opcao !=="5"){
+    console.log("\n-----Caixa Eletrônico-----");
+    console.log("1. Consultar Saldo");
+    console.log("2. Realizar Depósito");
+    console.log("3. Realizar Saldo");
+    console.log("4. Ver Extrato");
+    console.log("5. Sair / Relatório Final");
+
+    opcao = prompt("Escolha uma opção: ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
